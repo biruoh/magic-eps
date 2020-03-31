a675fa73b0f11e808da40721cc05b52a9c1c78e3
THE RISAZ <therisaz@pm.me>
2020-04-01 02:07:06 +0700

    Updates

---
+++ precache-manifest.85a91f071306d6804c9c02504e7bceff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883f51727a964a20a71d7ec49abec4db",
+    "url": "/magic-eps/index.html"
+  },
+  {
+    "revision": "f5348f51c13dd4a2d42b",
+    "url": "/magic-eps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8432b32c5eae2c6c7a5",
+    "url": "/magic-eps/static/js/2.9ac6d58b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magic-eps/static/js/2.9ac6d58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5348f51c13dd4a2d42b",
+    "url": "/magic-eps/static/js/main.409bc8fb.chunk.js"
+  },
+  {
+    "revision": "740cf24baa0d730943ab",
+    "url": "/magic-eps/static/js/runtime-main.6fcb2ad1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/magic-eps/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/magic-eps/precache-manifest.85a91f071306d6804c9c02504e7bceff.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/magic-eps/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
